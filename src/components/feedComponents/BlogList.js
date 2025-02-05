5c2e4cb2029b56ef82505fdea27dbee537d0f4eb
Ravi Agrahari <152021997+agravi987@users.noreply.github.com>
2025-02-05 14:29:02 +0530

    Update BlogList.js, backend ur changed

--- src/components/feedComponents/BlogList.js
+++ src/components/feedComponents/BlogList.js
@@ -10,7 +10,7 @@ const BlogList = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
+    fetch("https://risingsun-blog-server.onrender.com/blogs")
       .then((response) => response.json())
       .then((data) => setBlogs(data))
       .catch((error) => console.error("Error fetching blogs:", error));
