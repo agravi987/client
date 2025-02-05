dd99a4a9260aa009b770e47fb5e10a18a8d4901e
Ravi Agrahari <152021997+agravi987@users.noreply.github.com>
2025-02-05 14:26:14 +0530

    Update createblog.js , backend url changed

--- src/components/pages/createblog.js
+++ src/components/pages/createblog.js
@@ -39,7 +39,7 @@ const CreateBlog = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8000/blogs/create", {
+      const response = await fetch("https://risingsun-blog-server.onrender.com/blogs/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
